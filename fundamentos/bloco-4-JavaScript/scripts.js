9505e48d39d927f81534b9a06b2d5aa15ca2049d
fkamag <fabiokamagian@gmail.com>
2022-03-07 17:08:08 -0300

    parte 1 da aula 4.1

---
+++ fundamentos/bloco-4-JavaScript/scripts.js
@@ -0,0 +1,17 @@
+const myName = "Fábio";
+const birthCity = "São Paulo";
+let birthYear = 1970;
+
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Rio de Janeiro";
+
+console.log(birthCity);
+
