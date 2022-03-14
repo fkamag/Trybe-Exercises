3c1df89cce67309b4508a35a2820cf3a36e05f33
fkamag <fabiokamagian@gmail.com>
2022-03-14 19:40:31 -0300

    Segunda parte dos exercicios do dia 5.1

--- fundamentos/bloco-5-Dom-e-seletores/script.js
+++ fundamentos/bloco-5-Dom-e-seletores/script.js
@@ -4,3 +4,5 @@ const paragraph = document.getElementById("paragraph");
 paragraph.style.color = "red";
 document.getElementById('subtitle').innerText = 'Sinopse';
 document.getElementById("second-paragraph").innerText = "O filme se passa na Segunda Guerra Mundial onde Guido Orefice, um judeu dono de uma singela livraria judaica na Itália fascista, é capturado e mandado para um campo de concentração em Berlim, juntamente com seu filho, o pequeno Giosué; usando sua inteligência, espirituosidade e bom humor, Guido faz com que a criança acredite que ambos estão em um jogo, com o objetivo de protegê-lo do horror em que estão inseridos. La vita è bella foi parcialmente inspirado no livro Ho Sconfito Hitler, de Rubino Romeo Salmonì,[5] e por relatos verídicos vivido pelo próprio pai de Benigni, que passou dois anos em um campo de trabalho alemão durante a guerra."
+document.getElementsByClassName('paragrafo')[1].style.backgroundColor = "green";
+document.getElementsByTagName("h4")[0].style.color = "blue";
