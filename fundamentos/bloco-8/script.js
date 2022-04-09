3a2f5f78bdc4c8476f2b4cdc3a305b69ca999dc7
fkamag <fabiokamagian@gmail.com>
2022-04-09 00:00:08 -0300

    exercicios dia 8.2 imcompletos

--- fundamentos/bloco-8/script.js
+++ fundamentos/bloco-8/script.js
@@ -138,19 +138,42 @@
 
 // Adicione seu código aqui
 
-const numbers = [19, 21, 30, 3, 45, 22, 15];
+// const numbers = [19, 21, 30, 3, 45, 22, 15];
 
-const verifyEven = (number) => number % 2 === 0;
+// const verifyEven = (number) => number % 2 === 0;
 
-const isEven = numbers.find(verifyEven);
+// const isEven = numbers.find(verifyEven);
 
-console.log(isEven); // 30
+// console.log(isEven); // 30
 
-console.log(verifyEven(9)); // False
-console.log(verifyEven(14)); // True
+// console.log(verifyEven(9)); // False
+// console.log(verifyEven(14)); // True
 
-// Outra forma de ser realizada sem a necessidade de criar uma nova função.
-const isEven2 = numbers.find((number) => number % 2 === 0);
+// // Outra forma de ser realizada sem a necessidade de criar uma nova função.
+// const isEven2 = numbers.find((number) => number % 2 === 0);
 
-console.log(isEven2); // 30
+// console.log(isEven2); // 30
 
+// const numbers = [19, 21, 50, 32, 3, 45, 22, 15];
+
+// const findDivisibleBy3And5 = () => numbers.find((value) => value % 3 === 0 && value % 5 === 0 );
+
+// console.log(findDivisibleBy3And5());
+
+// const names = ['João','Maria', 'Irene', 'Fernando'];
+
+// const findNameWithFiveLetters = () => names.find((value) => value === "Fernando");
+
+// console.log(findNameWithFiveLetters());
+
+const musicas = [
+  { id: '31031685', title: 'Partita in C moll BWV 997' },
+  { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+]
+
+// const findMusic= (id) => musicas.find((value) => (value.id === id) ? `Sua música escolhida é ${value.title}` : `Não localizamos esta música.`);
+// console.log(findMusic('31031687'))
+
+const findMusic = (id) => musicas.find((musica) => musica.id === id );
+console.log(findMusic('31031686'));
