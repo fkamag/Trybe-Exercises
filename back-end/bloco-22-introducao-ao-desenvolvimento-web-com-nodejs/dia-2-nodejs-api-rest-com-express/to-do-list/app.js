50a39c27bb97330b44a7bfbf1b847d5c57995b87
fkamag <fabiokamagian@gmail.com>
2022-08-22 23:21:18 -0300

    iniciando to-do-list

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-api-rest-com-express/to-do-list/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const activities = [
+  {
+    id: 1,
+    description: 'Banho no cachorro',
+    status: 'A fazer',
+  },
+  {
+    id: 2,
+    description: 'Cortar a grama',
+    status: 'A fazer',
+  },
+  {
+    id: 3,
+    description: 'Estudar JavaScript',
+    status: 'Feito',
+  },
+];
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/teams', (req, res) => res.status(200).json({ activities }));
