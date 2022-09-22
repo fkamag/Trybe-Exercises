05f34b552b430299ae4dc6d554a46006f7ad66e1
fkamag <fabiokamagian@gmail.com>
2022-09-22 18:32:39 -0300

    criando tabela DB e Model

--- back-end/libraryapp/src/migrations/20220916220211-create-books.js
+++ back-end/libraryapp/src/migrations/20220916220211-create-books.js
@@ -2,17 +2,37 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('BooksModel', {
-      
-    })
+    await queryInterface.createTable('Books', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.dropTable('Books');
   }
 };
---
+++ back-end/libraryapp/src/models/books.model.js
@@ -0,0 +1,13 @@
+const BooksModel = (sequelize, DataTypes) => {
+  const Books = sequelize.define('Books', {
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    pageQuantity: DataTypes.INTEGER,
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  });
+
+  return Books;
+};
+
+module.exports = BooksModel;
