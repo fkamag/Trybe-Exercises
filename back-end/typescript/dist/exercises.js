f8668b5cd0168fa511543fa41a91c61ba8ccc7fb
fkamag <fabiokamagian@gmail.com>
2022-10-03 21:13:07 -0300

    primeiro programa com typescript

---
+++ back-end/typescript/dist/exercises.js
@@ -0,0 +1,45 @@
+"use strict";
+// ./exercises.ts
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.circle = exports.trapeze = exports.diamond = exports.rectangle = exports.square = exports.triangle = exports.sumArray = exports.add = exports.personAge = exports.greeter = void 0;
+function greeter(name) {
+    return `Olá ${name}!`;
+}
+exports.greeter = greeter;
+function personAge(name, age) {
+    return `${name} tem ${age} anos!`;
+}
+exports.personAge = personAge;
+function add(x, y) {
+    return x + y;
+}
+exports.add = add;
+function sumArray(numbers) {
+    return numbers.reduce(add, 0);
+}
+exports.sumArray = sumArray;
+function triangle(base, height) {
+    return (base * height) / 2;
+}
+exports.triangle = triangle;
+function square(side) {
+    return side ** 2;
+}
+exports.square = square;
+function rectangle(base, height) {
+    return base * height;
+}
+exports.rectangle = rectangle;
+function diamond(diagonal1, diagonal2) {
+    return (diagonal1 * diagonal2) / 2;
+}
+exports.diamond = diamond;
+function trapeze(baseMaior, baseMenor, altura) {
+    return (baseMaior + baseMenor) * altura / 2;
+}
+exports.trapeze = trapeze;
+function circle(radius) {
+    const pi = 3.14;
+    return pi * radius ^ 2;
+}
+exports.circle = circle;
